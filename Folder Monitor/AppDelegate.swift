19dc9a6d843ed2e7e3e0c3ab3825bbb2d9c6144d
iamthaoly <lytranthithao01@gmail.com>
2021-06-09 09:47:19 +0700

    First commit

---
+++ Folder Monitor/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Folder Monitor
+//
+//  Created by lytran on 6/9/21.
+//  Copyright © 2021 lytran. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Folder Monitor/ViewController.swift
@@ -0,0 +1,58 @@
+//
+//  ViewController.swift
+//  Folder Monitor
+//
+//  Created by lytran on 6/9/21.
+//  Copyright © 2021 lytran. All rights reserved.
+//
+
+import Cocoa
+import PDFKit
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        extractTextFromPDF()
+        // Do any additional setup after loading the view.
+    }
+    
+    // for test
+    func extractTextFromPDF() {
+//        let home = FileManager.default.homeDirectoryForCurrentUser
+//        let path = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+//        debugPrint(path)
+//        let pdfPath = home.appendingPathComponent("Documents/testfile.pdf")
+//        print(pdfPath)
+        if let pdfFileUrl = Bundle.main.url(forResource: "testfile", withExtension: "pdf") {
+            debugPrint("PDF file: \(pdfFileUrl)")
+            if let pdf = PDFDocument(url: pdfFileUrl) {
+                let pageCount = pdf.pageCount
+                print("PDF Number of page: \(pageCount)")
+                let content = pdf.string
+                print("PDF content: ")
+                debugPrint(content)
+                if let range = content?.range(of: "GERMANY") {
+                    let index = content?.distance(from: content!.startIndex, to: range.lowerBound)
+                    debugPrint(index)
+                }
+                
+            }
+            else {
+                debugPrint("Cannot read pdf!")
+            }
+        }
+        else {
+            debugPrint("PDF not found!")
+        }
+        
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
