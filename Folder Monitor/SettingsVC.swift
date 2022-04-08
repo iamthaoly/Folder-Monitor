e61cab3aa8f86fd92d555990113b176ce761b358
Ly <lyttt.dev@gmail.com>
2022-04-08 10:49:47 +0700

    Add settings vc

---
+++ Folder Monitor/SettingsVC.swift
@@ -0,0 +1,19 @@
+//
+//  SettingsVC.swift
+//  Folder Monitor
+//
+//  Created by ly on 08/04/2022.
+//  Copyright © 2022 lytran. All rights reserved.
+//
+
+import Cocoa
+
+class SettingsVC: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+        print("This is setting view controller.")
+    }
+    
+}
