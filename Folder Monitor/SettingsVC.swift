cfed4d500794c5da138d83a292d9ddeabb5724b3
Ly <lyttt.dev@gmail.com>
2022-04-11 11:16:43 +0700

    Save API Settings

--- Folder Monitor/SettingsVC.swift
+++ Folder Monitor/SettingsVC.swift
@@ -17,6 +17,15 @@ class SettingsVC: NSViewController {
     @IBOutlet weak var edtAPIKey: NSTextField!
     
     @IBOutlet weak var txtStatus: NSTextField!
+    // MARK: - CONSTANTS
+    let API_USERNAME_FIELD = "apiUsername"
+    let API_PASSWORD_FIELD = "apiPassword"
+    let API_KEY_FIELD = "apiKey"
+    
+    // MARK: - VARS
+    var username: String?
+    var password: String?
+    var apiKey: String?
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -26,10 +35,37 @@ class SettingsVC: NSViewController {
     
     
     @IBAction func saveAPISetting(_ sender: Any) {
+        if edtUsername.stringValue == "" || edtPassword.stringValue == "" || edtAPIKey.stringValue == "" {
+            // Display alert. Please fill all the fields.
+            
+        }
+        UserDefaults.standard.set(edtUsername.stringValue, forKey: API_USERNAME_FIELD)
+        UserDefaults.standard.set(edtPassword.stringValue, forKey: API_PASSWORD_FIELD)
+        UserDefaults.standard.set(edtAPIKey.stringValue, forKey: API_KEY_FIELD)
         
     }
     
     @IBAction func testAPI(_ sender: Any) {
-        
+        if loadAPI() == true {
+            
+        }
+
+    }
+    
+    private func loadAPI() -> Bool{
+        if let username = UserDefaults.standard.string(forKey: API_USERNAME_FIELD),
+           let password = UserDefaults.standard.string(forKey: API_PASSWORD_FIELD),
+           let apiKey = UserDefaults.standard.string(forKey: API_KEY_FIELD)
+           {
+            self.username = username
+            self.password = password
+            self.apiKey = apiKey
+            return true
+        }
+        else {
+            // Notice the user
+            // No API information had been saved. Please save first.
+            return false
+        }
     }
 }
